d3156778523b13c160e3fc4b5128fe1c5b775823
pedromolina17 <pedro.molinanoa@gmail.com>
2024-09-24 21:59:55 -0500

    Add Estatics SOunds

--- src/detail-sound/entities/detail-sound.entity.ts
+++ src/detail-sound/entities/detail-sound.entity.ts
@@ -1,12 +1,12 @@
 export class DetailSound {
-    id: number;
-    name: string;
-    url_youtube: string;
-    url_command: string;
-    date: Date;
-    likes: number;
-    duration: string;
-    categoryId: number;
-    imageCoverId: number;
-    soundId: number;
+  id: number;
+  name: string;
+  url_youtube: string;
+  url_command: string;
+  date: Date;
+  likes: number;
+  duration: string;
+  categoryId: number;
+  imageCoverId: number;
+  soundId: number;
 }
--- src/sound/sound.controller.ts
+++ src/sound/sound.controller.ts
@@ -1,14 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { SoundService } from './sound.service';
 import { CreateSoundDto } from './dto/create-sound.dto';
 import { UpdateSoundDto } from './dto/update-sound.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('sound')
 export class SoundController {
   constructor(private readonly soundService: SoundService) {}
 
   @Post()
-  create(@Body() createSoundDto: CreateSoundDto) {
+  @UseInterceptors(
+    FileInterceptor('sound_url', {
+      storage: diskStorage({
+        destination: './uploads/sounds',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `sound-${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() createSoundDto: CreateSoundDto,
+  ) {
+    const fileUrl = `/uploads/sounds/${file.filename}`;
+
+    createSoundDto.name = fileUrl;
+
     return this.soundService.create(createSoundDto);
   }
 
